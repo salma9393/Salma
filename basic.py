7a045a8853d0bb4e378b4f2fe75abfeb8fa26e31
salma9393 <shaik.salma786ms@gmail.com>
2025-09-07 11:39:26 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,66 @@
+print("ammu")
+print("Hello world!")
+print(" 'hello world'")
+print('"hello world" ')
+print('''hello world''')
+print('Hello world ')
+print(""" hello world""")
+print()
+print(''' "my name is 'ammu'" ''')
+print('my name is "ammu" ' )
+print(""" he said,"ammu's is very good girl", i satisfy """)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
