71b260efc70f498f60efa449f845fb75dd149089
salma9393 <shaik.salma786ms@gmail.com>
2025-09-17 10:50:19 +0530

    break, continue, pass

---
+++ pass.py
@@ -0,0 +1,25 @@
+#break
+i=1
+while i <=10:
+    if i==5:
+        break
+    print(i)
+    i+=1
+
+#continue
+i=0
+while i <=10:
+    i+=1
+    if i==7:
+        continue
+    print(i)
+    
+
+#pass
+i=0
+while i <=10:
+    if i==6:
+        pass
+    print(i)
+    i+=1
+
