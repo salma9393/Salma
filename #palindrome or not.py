9ad3733b30c3ef08c0c58488a54c53576e26ea4d
SHAIK SALMA <shaik.salma786ms@gmail.com>
2025-10-27 07:04:55 +0530

    #palindrome or not

---
+++ #palindrome or not.py
@@ -0,0 +1,16 @@
+#palindrome or not
+str="madam"
+if str==str[::-1]:
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+#palindrome or not
+s="madam"
+print(s==s[::-1])
+
+
+
+#palindrome or not
+s="madam"
+print("palindrome" if s==s[::-1] else "not a palindrome")
