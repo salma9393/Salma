a9257bb94276fb05aeed4d6157e5431b7ca3820f
SHAIK SALMA <shaik.salma786ms@gmail.com>
2025-10-28 07:22:57 +0530

    #max of 3 numbers

---
+++ #max of 3 numbers.py
@@ -0,0 +1,24 @@
+#max of 3 numbers
+a=2
+b=4
+c=6
+if a>b and a>c:
+    print("a is big",a)
+elif b>c and b>a:
+    print("b is big",b)
+else:
+    print("c is big",c)
+
+
+# max of 3 numbers
+a=2
+b=4
+c=9
+if a>b and a>c:
+    max=a
+elif b>a and b>c:
+    max=b
+else:
+    max=c
+print(max)
+
