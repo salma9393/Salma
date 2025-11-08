58c60c7c60a83c5ab93a2427621df7cc60b06f01
SHAIK SALMA <shaik.salma786ms@gmail.com>
2025-11-08 21:00:03 +0530

    #for loop using range

---
+++ #for loop using range().py
@@ -0,0 +1,12 @@
+#for loop using range()
+for i in range(1,11):
+    print(i)
+
+for i in range(0,11,2):
+    print(i)
+
+for i in range(1,11,2):
+    print(i)
+
+for i in range(0,101,2):
+    print(i)
