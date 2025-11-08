1ef74d3ced799738824d46ef29eae7289c49e53d
SHAIK SALMA <shaik.salma786ms@gmail.com>
2025-11-08 21:07:13 +0530

    #even numbers &odd numbers

---
+++ #even numbers&odd numbers.py
@@ -0,0 +1,77 @@
+#even numbers
+n=10
+for i in range(1,n+1):
+    print(i*2)
+
+
+
+#odd numbers
+n=10
+for i in range(1,n+1):
+    print(i*2-1,end=" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
