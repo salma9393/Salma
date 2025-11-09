b9d73834bcb58219e7e0b2163fa1bd9b177ee0e8
SHAIK SALMA <shaik.salma786ms@gmail.com>
2025-11-09 11:44:46 +0530

    #sum of n numbers

---
+++ #sum on n numbers.py
@@ -0,0 +1,25 @@
+#sum on n numbers
+n=10
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum)
+
+
+
+#sum of even numbers
+n=10
+sum=0
+for i in range(2,n+1,2):
+    sum=sum+i
+print(sum)
+
+
+
+#sum of odd numbers
+n=10
+sum=0
+for i in range(1,n+1,2):
+    sum=sum+i
+print(sum)
+
